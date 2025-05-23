0df833106882dadf1f5a589738bb607e3eb1ffd7
Chase Blodgett <blodg072@umn.edu>
2025-05-23 10:32:18 -0500

    postorder solution

---
+++ postorder.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorder_rec(self, root, res):
+        if not root:
+            return
+        
+        
+        self.postorder_rec(root.left, res)
+        self.postorder_rec(root.right, res)
+        res.append(root.val)
+
+        
+
+    def postorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        res = []
+        self.postorder_rec(root, res)
+        return res
