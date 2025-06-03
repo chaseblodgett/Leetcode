353423cd148696a8775b516fc07cb2f9078f2fc9
Chase Blodgett <blodg072@umn.edu>
2025-06-03 10:01:48 -0500

    capture surrounding solution

---
+++ capture_surrounding.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        
+        if not board:
+            return
+
+        m, n = len(board), len(board[0])
+
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= m or j >= n or board[i][j] != 'O':
+                return
+            board[i][j] = 'T'
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n-1)
+        for j in range(n):
+            dfs(0, j)
+            dfs(m-1, j)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'T':
+                    board[i][j] = 'O'
+
+        
+
