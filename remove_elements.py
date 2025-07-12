4c6d6ec52ebeb44681c765abfbabd0bbfbb98fd4
chaseblodgett <blodg072@umn.edu>
2025-07-12 12:21:57 -0500

    remove elements solution

---
+++ remove_elements.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: Optional[ListNode]
+        :type val: int
+        :rtype: Optional[ListNode]
+        """
+        while head and head.val == val:
+            head = head.next
+
+        prev = head
+        curr = head
+        while curr:
+            
+            if curr.val == val:
+                prev.next = curr.next
+            else:
+                prev = curr
+            
+            curr = curr.next
+        
+        if prev and prev.val == val:
+            prev = None
+
+        return head
